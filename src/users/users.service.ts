e1ad1fe84f62121dc6a3026fd423445e28145572
Yuliya Khazran <ykhazran@gmail.com>
2021-03-24 22:34:38 +0200

    users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,7 +8,18 @@ export type User = any;
 export class UsersService {
   constructor(private config: ConfigService) {}
 
-  private readonly users = [];
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'superadmin',
+      password: 'superadmin',
+    },
+  ];
 
   async findOne(username: string): Promise<User | undefined> {
     this.users.push({
